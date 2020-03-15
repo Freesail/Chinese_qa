14a498e674bc9a9b329aa5662419875bb210faa5
Freesail <imperialfreesail@gmail.com>
2020-03-15 15:11:42 +0000

    initial commit

---
+++ BiDAF.py
@@ -0,0 +1,223 @@
+from torch import nn
+import torch
+import torch.nn.functional as F
+
+
+class LSTM(nn.Module):
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 num_layers=1,
+                 batch_first=True,
+                 bidirectional=False,
+                 lstm_dropout=0.0,
+                 input_dropout=0.2
+                 ):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=batch_first,
+            dropout=lstm_dropout,
+            bidirectional=bidirectional
+        )
+        self.dropout = nn.Dropout(p=input_dropout)
+
+    def forward(self, x, x_len):
+        # x should have descending seq_len
+        x = self.dropout(x)
+        x_packed = nn.utils.rnn.pack_padded_sequence(
+            input=x,
+            lengths=x_len,
+            batch_first=True,
+            enforce_sorted=True
+        )
+        out_packed, (h_n, c_n) = self.lstm(x_packed)
+        out, _ = nn.utils.rnn.pad_packed_sequence(
+            out_packed,
+            batch_first=True
+        )
+        return out
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, input_dropout=0.0):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(in_features=in_features, out_features=out_features)
+        self.dropout = nn.Dropout(p=input_dropout)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        x = self.linear(x)
+        return x
+
+
+# TODO: finisth this part
+class CTXEmbedding(nn.Module):
+    def __init__(self, encoder):
+        super(CTXEmbedding, self).__init__()
+        self.encoder = encoder
+
+    def forward(self, x, x_len):
+        return self.encoder(x, x_len)[0]
+
+
+class HignWay(nn.Module):
+    def __init__(self, d):
+        super(HignWay, self).__init__()
+        self.linear_layers = []
+        self.gate_layers = []
+        for i in range(2):
+            self.linear_layers.append(
+                nn.Sequential(Linear(2 * d, 2 * d), nn.ReLU())
+            )
+            self.gate_layers.append(
+                nn.Sequential(Linear(2 * d, 2 * d), nn.Sigmoid())
+            )
+
+    def forward(self, x):
+        # x = torch.cat([x1, x2], dim=-1)
+        for i in range(2):
+            h = self.linear_layers[i](x)
+            g = self.gate_layers[i](x)
+            x = g * h + (1 - g) * x
+        return x
+
+
+class AttentionFlow(nn.Module):
+    def __init__(self, d):
+        super(AttentionFlow, self).__init__()
+        self.weight_c = Linear(2 * d, 1)
+        self.weight_q = Linear(2 * d, 1)
+        self.weight_cq = Linear(2 * d, 1)
+
+    def forward(self, c, q):
+        c_len = c.size(1)
+        q_len = q.size(1)
+
+        cq = []
+        for i in range(q_len):
+            # (batch, 1, hidden_size * 2)
+            qi = q.select(1, i).unsqueeze(1)
+            # (batch, c_len, 1)
+            ci = self.weight_cq(c * qi).squeeze()
+            cq.append(ci)
+        # (batch, c_len, q_len)
+        cq = torch.stack(cq, dim=-1)
+
+        # (batch, c_len, q_len)
+        s = self.weight_c(c).expand(-1, -1, q_len) + \
+            self.weight_q(q).permute(0, 2, 1).expand(-1, c_len, -1) + \
+            cq
+
+        # (batch, c_len, q_len)
+        a = F.softmax(s, dim=2)
+        # (batch, c_len, q_len) * (batch, q_len, hidden_size * 2) -> (batch, c_len, hidden_size * 2)
+        c2q_att = torch.bmm(a, q)
+        # (batch, 1, c_len)
+        b = F.softmax(torch.max(s, dim=2)[0], dim=1).unsqueeze(1)
+        # (batch, 1, c_len) * (batch, c_len, hidden_size * 2) -> (batch, hidden_size * 2)
+        q2c_att = torch.bmm(b, c).squeeze()
+        # (batch, c_len, hidden_size * 2) (tiled)
+        q2c_att = q2c_att.unsqueeze(1).expand(-1, c_len, -1)
+        # q2c_att = torch.stack([q2c_att] * c_len, dim=1)
+
+        # (batch, c_len, hidden_size * 8)
+        x = torch.cat([c, c2q_att, c * c2q_att, c * q2c_att], dim=-1)
+        return x
+
+
+class QAOutput(nn.Module):
+    def __init__(self, d, dropout):
+        super(QAOutput, self).__init__()
+        self.linear_p1 = Linear(10 * d, 1, input_dropout=dropout)
+        self.linear_p2 = Linear(10 * d, 1, input_dropout=dropout)
+        self.output_lstm = LSTM(
+            input_size=2 * d,
+            hidden_size=d,
+            bidirectional=True,
+            input_dropout=dropout
+        )
+
+    def forward(self, g, m, l):
+        gm = torch.cat([g, m], dim=-1)
+        p1 = self.linear_p1(gm).squeeze()
+        m2 = self.output_lstm(m, l)
+        gm2 = torch.cat([g, m2], dim=-1)
+        p2 = self.linear_p2(gm2).squeeze()
+        return p1, p2
+
+
+class BiDAF(nn.Module):
+    def __init__(self,
+                 word_emb,
+                 cxt_emb):
+        super(BiDAF, self).__init__()
+
+        # 1a. word Embedding layer
+        self.word_emb = nn.Embedding.from_pretrained(word_emb, freeze=True)
+        # 1b. cxt Embedding layer
+        self.cxt_emb = None
+        # TODO: sum to hidden dim
+        self.hidden_dim = 10
+
+        # 2. Highway
+        self.highway = HignWay(d=self.hidden_dim)
+
+        # 3. Contextual Embedding Layer
+        self.context_lstm = LSTM(
+            input_size=self.hidden_dim * 2,
+            hidden_size=self.hidden_dim,
+            bidirectional=True,
+            input_dropout=args.dropout)
+
+        self.attention_flow = AttentionFlow(d=self.hidden_dim)
+
+        self.modeling_lstm = LSTM(
+            input_size=self.hidden_dim * 8,
+            hidden_size=self.hidden_dim,
+            num_layers=2,
+            bidirectional=True,
+            input_dropout=args.dropout
+        )
+
+        self.qa_output = QAOutput(
+            d=self.hidden_dim,
+            dropout=args.dropout
+        )
+
+    def forward(self, batch):
+        context, context_len = batch.context[0], batch.context[1]
+        question, question_len = batch.question[0], batch.question[1]
+
+        # 1a. word Embedding Layer
+        c = self.word_emb(context)
+        q = self.word_emb(question)
+        # 1b. cxt Embedding Layer
+        if self.cxt_emb is not None:
+            # TODO
+            c_cxt = self.cxt_emb(context, context_len)
+            q_cxt = self.cxt_emb(question, question_len)
+            c = torch.cat([c, c_cxt], dim=-1)
+            q = torch.cat([q, q_cxt], dim=-1)
+
+        # 2. Highway network
+        c = self.highway(c)
+        q = self.highway(q)
+
+        # 3. Contextual Embedding Layer
+        c = self.context_LSTM(c, context_len)
+        q = self.context_LSTM(q, question_len)
+
+        # 4. Attention Flow Layer
+        g = self.attention_flow(c, q)
+
+        # 5. Modeling Layer
+        m = self.modeling_LSTM(g, context_len)
+
+        # 6. Output Layer
+        p1, p2 = self.qa_output(g, m, context_len)
+
+        # (batch, c_len), (batch, c_len)
+        return p1, p2
---
+++ DataPipeline.py
@@ -0,0 +1,228 @@
+from torchtext import data
+import os
+import torch
+from torchtext import vocab
+from torchtext.vocab import GloVe
+import nltk
+import json
+import jieba
+import re
+
+
+class DataPipeline:
+    def __init__(self, raw_examples,
+                 processed_examples,
+                 saved_datasets,
+                 language='English',
+                 context_threshold=-1):
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.language = language
+        if self.language == 'English':
+            self.tokenize = lambda x: \
+                [token.replace("''", '"').replace("``", '"') for token in nltk.word_tokenize(x)]
+            self.word_vector = GloVe(name='6B', dim=100)
+        else:
+            self.tokenize = lambda x: list(jieba.cut(x, cut_all=False))
+            self.word_vector = vocab.Vectors('Tencent_AILab_ChineseEmbedding.txt')
+
+        self.context_threshold = context_threshold
+        self.processed_examples = processed_examples
+        if not os.path.exists(processed_examples):
+            print('preprocess raw examples')
+            if self.language == 'English':
+                self.process_eng(raw_examples, processed_examples)
+            else:
+                self.process_ch(raw_examples, processed_examples)
+
+        self.id_type = data.RawField(is_target=False)
+        if self.language == 'English':
+            self.word_type = data.Field(batch_first=True, include_lengths=True,
+                                        tokenize=self.tokenize, lower=True)
+        else:
+            self.word_type = data.Field(batch_first=True, include_lengths=True,
+                                        tokenize=self.tokenize)
+        self.idx_type = data.Field(sequential=False, use_vocab=False, unk_token=None)
+
+        self.fields = {
+            'id': ('id', self.id_type),
+            'context': ('context', self.word_type),
+            'question': ('question', self.word_type),
+            's_idx': ('s_idx', self.idx_type),
+            'e_idx': ('e_idx', self.idx_type),
+        }
+
+        if os.path.exists(saved_datasets):
+            print('load datasets')
+            self.datasets = self.load_datasets(saved_datasets)
+        else:
+            print('generate and save datasets')
+            datasets = data.TabularDataset(
+                path=self.processed_examples,
+                format='json',
+                fields=self.fields,
+            ).split(split_ratio=0.7)
+
+            if self.context_threshold > 0:
+                datasets[0].examples = \
+                    [e for e in datasets[0].examples if len(e.context) <= self.context_threshold]
+
+            self.datasets = {
+                'train': datasets[0],
+                'val': datasets[1]
+            }
+            self.save_datasets(saved_datasets)
+
+        print('generate_vocab')
+        self.word_type.build_vocab(*self.datasets.values(), vectors=self.word_vector)
+
+        print('generate iterator')
+        self.data_iterators = dict()
+        self.data_iterators['train'] = data.BucketIterator(
+            self.datasets['train'],
+            batch_size=1,
+            sort_key=lambda x: len(x.context),
+            train=True,
+            repeat=True,
+            shuffle=True,
+            sort=True,
+            device=self.device,
+        )
+        self.data_iterators['val'] = data.BucketIterator(
+            self.datasets['val'],
+            batch_size=1,
+            sort_key=lambda x: len(x.context),
+            repeat=True,
+            sort=True,
+            device=self.device,
+        )
+
+    def save_datasets(self, saved_datasets):
+        examples = {
+            x: self.datasets[x].examples
+            for x in ['train', 'val']
+        }
+        torch.save(examples, saved_datasets)
+
+    def load_datasets(self, saved_datasets):
+        examples = torch.load(saved_datasets)
+        datasets = {
+            x: data.Dataset(examples=examples[x], fields=list(self.fields.values()))
+            for x in ['train', 'val']
+        }
+        return datasets
+
+    def process_eng(self, raw_examples, processed_examples):
+        dump = []
+        abnormals = [' ', '\n', '\u3000', '\u202f', '\u2009']
+
+        with open(raw_examples, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            data = data['data']
+
+            for article in data:
+                for paragraph in article['paragraphs']:
+                    context = paragraph['context']
+                    tokens = self.tokenize(context)
+                    for qa in paragraph['qas']:
+                        id = qa['id']
+                        question = qa['question']
+                        for ans in qa['answers']:
+                            answer = ans['text']
+                            s_idx = ans['answer_start']
+                            e_idx = s_idx + len(answer)
+
+                            l = 0
+                            s_found = False
+                            for i, t in enumerate(tokens):
+                                while l < len(context):
+                                    if context[l] in abnormals:
+                                        l += 1
+                                    else:
+                                        break
+                                # exceptional cases
+                                if t[0] == '"' and context[l:l + 2] == '\'\'':
+                                    t = '\'\'' + t[1:]
+                                elif t == '"' and context[l:l + 2] == '\'\'':
+                                    t = '\'\''
+
+                                l += len(t)
+                                if l > s_idx and s_found == False:
+                                    s_idx = i
+                                    s_found = True
+                                if l >= e_idx:
+                                    e_idx = i
+                                    break
+
+                            dump.append(dict([('id', id),
+                                              ('context', context),
+                                              ('question', question),
+                                              ('answer', answer),
+                                              ('s_idx', s_idx),
+                                              ('e_idx', e_idx)]))
+
+        with open(processed_examples, 'w', encoding='utf-8') as f:
+            for line in dump:
+                json.dump(line, f)
+                print('', file=f)
+
+    def process_ch(self, raw_examples, processed_examples):
+
+        def regex_change(line):
+            eng_regex = re.compile(r'[a-zA-Z]')
+            punc_regex = re.compile(r'[\W]')  # ("[" + re.escape(string.punctuation) + "]")
+            decimal_regex = re.compile(r'[\d]')
+
+            line = punc_regex.sub(r"", line)
+            line = decimal_regex.sub(r"", line)
+            line = eng_regex.sub(r"", line)
+
+            return line
+
+        def find_sub_list(sl, l):
+            sll = len(sl)
+            i = len(l)
+            for idx, ele in enumerate(l):
+                if ele == sl[0]:
+                    if l[idx:idx + sll] == sl:
+                        i = idx
+                        break
+            l = l[:i + sll] + '\u5929\u5b89\u95e8 ' + l[i + sll:]
+            l = l[:i] + '\u9955\u992e' + l[i:]
+
+            l = self.tokenize(l)
+            end = l.index('\u5929\u5b89\u95e8')
+            start = l.index('\u9955\u992e')
+            return start + 1, end
+
+        dump = []
+        with open(raw_examples, 'r') as f:  # , encoding='utf-8'
+            raw_data = json.load(f)
+            raw_data = raw_data['data']
+            for article in raw_data:
+                for paragraph in article['paragraphs']:
+                    context = paragraph['context']
+                    context = regex_change(context)
+                    con_tokens = self.tokenize(context)
+                    if len(con_tokens) > 3:
+                        for qa in paragraph['qas']:
+                            example_id = qa['id']
+                            question = qa['question']
+                            for ans in qa['answers']:
+                                answer = ans['text']
+                                answer = regex_change(answer)
+                                try:
+                                    (s_idx, e_idx) = find_sub_list(answer, context)
+                                    dump.append(dict([('id', example_id),
+                                                      ('context', context),
+                                                      ('question', question),
+                                                      ('answer', answer),
+                                                      ('s_idx', s_idx),
+                                                      ('e_idx', e_idx)]))
+                                except (TypeError, IndexError) as error:
+                                    pass
+
+        with open(processed_examples, 'w') as f:
+            for line in dump:
+                json.dump(line, f)
+                print('', file=f)
